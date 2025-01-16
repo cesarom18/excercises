71fca457ab40474a9e2adfe40b99fa779b53ad78
cesarom18 <cesarosses885@gmail.com>
2025-01-16 15:19:38 -0300

    actualizacion de script para permitir crear archivos en distintos directorios

--- newExercise.js
+++ newExercise.js
@@ -8,7 +8,7 @@ const test1 = null;
 
 ${process.argv[2]}(test1);`;
 
-fs.writeFile(`./codewars/${process.argv[2]}.js`, content, err => {
+fs.writeFile(`./${process.argv[3]}/${process.argv[2]}.js`, content, err => {
     if (err) {
         console.error(`Error Al Crear Archivo`);
     } else {
