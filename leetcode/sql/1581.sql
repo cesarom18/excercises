e721d2f64d2a080b0ed6173fcb59f396afce4bba
cesarom18 <cesarosses885@gmail.com>
2025-02-16 13:28:16 -0300

    1581 (SQL) Completado

---
+++ leetcode/sql/1581.sql
@@ -0,0 +1,32 @@
+-- +-------------+---------+
+-- | Column Name | Type    |
+-- +-------------+---------+
+-- | visit_id    | int     |
+-- | customer_id | int     |
+-- +-------------+---------+
+-- visit_id is the column with unique values for this table.
+-- This table contains information about the customers who visited the mall.
+
+-- +----------------+---------+
+-- | Column Name    | Type    |
+-- +----------------+---------+
+-- | transaction_id | int     |
+-- | visit_id       | int     |
+-- | amount         | int     |
+-- +----------------+---------+
+-- transaction_id is column with unique values for this table.
+-- This table contains information about the transactions made during the visit_id.
+
+-- Write a solution to find the IDs of the users who visited without making any transactions and the number of times they made these types of visits.
+-- Return the result table sorted in any order.
+
+WITH NoTransactionVisits AS (
+    SELECT v.customer_id
+    FROM Visits v
+    LEFT JOIN Transactions t ON v.visit_id = t.visit_id
+    WHERE t.transaction_id IS NULL
+)
+
+SELECT customer_id, COUNT(*) AS count_no_trans
+FROM NoTransactionVisits
+GROUP BY customer_id;
